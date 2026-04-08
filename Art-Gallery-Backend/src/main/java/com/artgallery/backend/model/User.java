3c72364d35793456e2c2ec819394988731b13d0f
MATSA BHARATH KUMAR <bharathkumarmatsa@gmail.com>
2026-04-08 14:19:43 +0530

    30708 updated user class

--- Art-Gallery-Backend/src/main/java/com/artgallery/backend/model/User.java
+++ Art-Gallery-Backend/src/main/java/com/artgallery/backend/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private String name;
 
     @Column(nullable = false)
-    private String role; // visitor, artist, curator, admin
+    private String role;
 
     public User() {}
 
